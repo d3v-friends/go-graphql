a2569a61319f7b16d29474923ec724b3c51efa47
CiaoLee <ciaolee87@gmail.com>
2024-01-28 11:50:54 +0900

    1. save

---
+++ fn/fnGql/echo.go
@@ -0,0 +1,59 @@
+package fnGql
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/d3v-friends/go-tools/fn/fnPanic"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+	"strings"
+)
+
+const CtxHeader = "CTX_HEADER"
+
+var ErrNotFoundHeader = errors.New("not found header")
+
+func NewEcho() (e *echo.Echo) {
+	e = echo.New()
+	e.Use(middleware.Gzip())
+	e.Use(middleware.CORS())
+	return
+}
+
+func Launch(e *echo.Echo, port string) {
+	if !strings.HasPrefix(port, ":") {
+		port = fmt.Sprintf(":%s", port)
+	}
+
+	fnPanic.On(e.Start(port))
+}
+
+func HeaderMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			var ctx = c.Request().Context()
+			ctx = context.WithValue(ctx, CtxHeader, c.Request().Header)
+			c.SetRequest(c.Request().WithContext(ctx))
+			return next(c)
+		}
+	}
+}
+
+func GetHeader(ctx context.Context) (header *http.Header, err error) {
+	var isOk bool
+	if header, isOk = ctx.Value(CtxHeader).(*http.Header); !isOk {
+		err = ErrNotFoundHeader
+		return
+	}
+	return
+}
+
+func GetHeaderP(ctx context.Context) (header *http.Header) {
+	var err error
+	if header, err = GetHeader(ctx); err != nil {
+		panic(err)
+	}
+	return
+}
--- go.mod
+++ go.mod
@@ -5,12 +5,24 @@ go 1.21.6
 require (
 	github.com/99designs/gqlgen v0.17.43
 	github.com/d3v-friends/go-tools v0.0.1
+	github.com/labstack/echo/v4 v4.11.4
 	github.com/shopspring/decimal v1.3.1
 	go.mongodb.org/mongo-driver v1.13.1
 )
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/sosodev/duration v1.1.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/vektah/gqlparser/v2 v2.5.11 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
